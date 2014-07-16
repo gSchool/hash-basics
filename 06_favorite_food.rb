f8350a6f43d31519fff931e1e160dc5a95f8bd73
Jeff Taggart <jeff@jetaggart.com>
2014-07-16 14:49:37 -0600

    Add favorite foods

--- 06_favorite_food.rb
+++ 06_favorite_food.rb
@@ -1,3 +1,6 @@
 require_relative "person"
 
 # Print Bjorn's favorite foods. It should read "Bjorn's favorite foods are sushi, hamburgers, and mexican food."
+
+favorite_foods = BJORN_BORG["favorite_foods"].join(', ')
+puts "Bjorn's favorite foods are #{favorite_foods}"
