858d8362dd4482fb69c8aa1350621aad135bf40d
Jeff Taggart <jeff@jetaggart.com>
2014-07-16 14:45:38 -0600

    Print address

--- 05_print_address.rb
+++ 05_print_address.rb
@@ -1,3 +1,6 @@
 require_relative "person"
 
 # Print Bjorn's address. It should read "444 Borg Lane, San Francisco, CA, 94104"
+address = BJORN_BORG["address"]
+
+puts "#{address["street"]}, #{address["city"]}, #{address["state"]}, #{address["zip_code"]}"
