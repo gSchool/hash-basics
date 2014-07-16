70f9bb408946d6e659412602e7e7722663f2d4d7
Jeff Taggart <jeff@jetaggart.com>
2014-07-16 14:43:29 -0600

    Greet bjorn borg

--- 03_greeting.rb
+++ 03_greeting.rb
@@ -1,4 +1,4 @@
 require_relative "person"
 
 # Print the string "Hello, nice to meet you Bjorn Borg."
-puts "Hello, nice to meet you #{BJORN_BORG['first_name']} #{BJORN_BORG['last_name']}."
+puts "Hello, nice to meet you #{BJORN_BORG['first_name']} #{BJORN_BORG['last_name']}"
